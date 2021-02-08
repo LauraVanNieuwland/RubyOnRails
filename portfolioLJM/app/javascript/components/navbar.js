394f20aceba0cd6f1712baaeb80633630dcce764
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-02-08 16:13:47 +0100

    Add sticky navbar

---
+++ portfolioLJM/app/javascript/components/navbar.js
@@ -0,0 +1,21 @@
+const stickyNavbar = () => {
+  let scrollpos = window.scrollY
+  const header = document.querySelector("#navbar")
+  const header_height = header.offsetHeight
+  
+  
+  const add_class_on_scroll = () => header.classList.add("sticky")
+  const remove_class_on_scroll = () => header.classList.remove("sticky")
+  
+  
+  window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY
+  
+    if (scrollpos >= header_height) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+  
+    console.log(scrollpos)
+  })
+} 
+
+ export { stickyNavbar };
